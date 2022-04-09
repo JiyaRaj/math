e5181a8680d036d21763f890d403ff02b6f3868b
JiyaRaj <86504048+JiyaRaj@users.noreply.github.com>
2022-04-08 20:04:29 -0500

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,7 +1,8 @@
-function Start(){
-   player1=document.getElementById("player_1").value;
-   player2=document.getElementById("player_2").value;
-   localStorage.setItem("name1", player1);
-   localStorage.setItem("name2", player2);
-   window.location="game_page.html";
-}
+function adduser(){
+    player1=document.getElementById("player_1").value;
+    player2=document.getElementById("player_2").value;
+    localStorage.setItem("name1", player1);
+    localStorage.setItem("name2", player2);
+    window.location="game.html";
+ }
+
